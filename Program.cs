e57020f301b8353f5aadeedd137482e616875fe1
tmdgus0245 <tmdgus0245@ajou.ac.kr>
2026-03-17 20:53:55 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+
+namespace C51
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            WriteLine("여러분, 안녕하세요?\n반갑습니다!");
+        }
+    }
+}
